4d8f0033d0747af4ef48dbd9c0b8bb57309a0041
JonasMS <jonas.sota@gmail.com>
2022-02-06 13:47:17 -0700

    impl interface

--- contracts/interfaces/IRevenueSplitter.sol
+++ contracts/interfaces/IRevenueSplitter.sol
@@ -2,9 +2,64 @@
 pragma solidity >=0.8.4;
 
 interface IRevenueSplitter {
+    function balanceOfUnexercised(address account_) external view returns (uint256);
+
+    function deposit() external payable;
+
+    function withdraw() external;
+
+    function withdrawBySig(
+        uint256 revenuePeriodDate_,
+        uint8 v_,
+        bytes32 r_,
+        bytes32 s_
+    ) external;
+
+    function withdrawBulk(
+        uint256[] calldata datesList_,
+        uint8[] calldata vList_,
+        bytes32[] calldata rList_,
+        bytes32[] calldata sList_
+    ) external;
+
+    function redeem() external;
+
+    function redeemBySig(
+        uint256 revenuePeriodDate_,
+        uint8 v_,
+        bytes32 r_,
+        bytes32 s_
+    ) external;
+
+    function redeemBulk(
+        uint256[] calldata datesList_,
+        uint8[] calldata vList_,
+        bytes32[] calldata rList_,
+        bytes32[] calldata sList_
+    ) external;
+
+    function endPeriod() external;
+
+    function setMaxTokenSupply(uint256 maxTokenSupply_) external;
+
     function execute(bytes calldata data_) external;
 
-    // receive() external payable;
+    event Deposit(address indexed account, uint256 amount);
+
+    event Withdraw(address indexed account, uint256 amount);
+
+    event MintRestricted(address indexed account, uint256 amount);
+
+    event Redeem(address indexed account, uint256);
+
+    event StartNewPeriod(
+        uint256 indexed periodId,
+        uint256 periodDate,
+        uint256 periodRevenue,
+        uint256 periodTotalSupply
+    );
+
+    event Execute(address indexed target, uint256 value, bytes);
 
     event PaymentReceived(address, uint256);
 }
